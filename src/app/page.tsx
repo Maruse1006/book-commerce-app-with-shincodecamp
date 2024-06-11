75900d41a3827a2bf542964013a964aee418c2f9
Maruse1006 <oonnwu9119@yahoo.ne.jp>
2024-06-12 08:39:42 +0900

    mod

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,7 +54,7 @@ export default async function Home() {
           <Book
             key={book.id}
             book={book}
-            user={user}
+            // user={user}
             isPurchased={false}
           />
         ))}
